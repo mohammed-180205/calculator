f81f956ec47951dce60d1da8f375e58303a69808
mohammed-180205 <mohammed.180205@gmail.com>
2026-05-16 16:19:56 +0300

    adding event listeners and storing the user input to variables

--- main.js
+++ main.js
@@ -14,8 +14,9 @@ function divide(a, b){
 return a / b;
 }
 
-
-let operator = "*";
+let num1 = "";
+let num2 = "";
+let operator = "";
 
 function operate(num1, operator, num2){
 switch(operator){
@@ -32,4 +33,48 @@ switch(operator){
         return divide(num1,num2);
 }
 }
+
+let setState = true;
+let inputBox = document.querySelector("input");
+let num1btn = document.querySelectorAll(".number");
+num1btn.forEach((item) => {
+item.addEventListener("click", () => {
+    if(setState){
+    num1 += item.textContent;
+} else{
+    num2 += item.textContent;
+}
+    inputBox.value += item.textContent;
+    
+    
+
+})
+})
+
+let operatorBtn = document.querySelectorAll(".operator");
+operatorBtn.forEach((item) => {
+item.addEventListener("click", () => {
+    setState = false;
+    operator = item.textContent;
+    inputBox.value += item.textContent;
+    
+    
+
+})
+})
+
+let equalSign = document.querySelector(".equalTo");
+
+equalSign.addEventListener("click", () => {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    let result = operate(num1, operator, num2);
+inputBox.value = result;
+num1 = result;
+num2 = "";
+});
+
+
+
+
 
